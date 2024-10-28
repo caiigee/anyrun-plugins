d010474520e1c218bfbf59505335e4283b0b381d
caiigee <141404025+caiigee@users.noreply.github.com>
2024-10-28 13:34:37 +0100

    4.2

--- flake.nix
+++ flake.nix
@@ -1,30 +1,32 @@
 {
   description = "Custom anyrun plugins";
-
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
-
   outputs = { nixpkgs, ... }:
     let
       forAllSystems = nixpkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" ];
-    in
-    forAllSystems (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        default-package = pkgs.rustPlatform.buildRustPackage {
-          pname = "anyrun-plugins";
-          version = "0.1.0";
-          src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-            outputHashes = {
-              "anyrun-interface-0.1.0" = "sha256-fQ4LkmZeW4eGowbVfvct1hLFD0hNkZiX5SzRlWqhgxc=";
-            };
+      
+      makePackage = pkgs: pkgs.rustPlatform.buildRustPackage {
+        pname = "anyrun-plugins";
+        version = "0.1.0";
+        src = ./.;
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+          outputHashes = {
+            "anyrun-interface-0.1.0" = "sha256-fQ4LkmZeW4eGowbVfvct1hLFD0hNkZiX5SzRlWqhgxc=";
           };
-          buildInputs = with pkgs; [ sqlite ];
         };
-      in {
-        packages.${system}.default = default-package;
-        devShells.${system} = pkgs.callPackage ./shell.nix { inherit default-package; };
-      }
-    );
+        buildInputs = with pkgs; [ sqlite ];
+      };
+    in
+    {
+      packages = forAllSystems (system: {
+        default = makePackage nixpkgs.legacyPackages.${system};
+      });
+
+      devShells = forAllSystems (system: {
+        default = nixpkgs.legacyPackages.${system}.callPackage ./shell.nix {
+          default-package = makePackage nixpkgs.legacyPackages.${system};
+        };
+      });
+    };
 }
